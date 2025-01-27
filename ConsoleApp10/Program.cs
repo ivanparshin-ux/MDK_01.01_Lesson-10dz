a0d4a396551836195942b7eb9e871b965c64fbba
Levikaci <121298029+Levikaci@users.noreply.github.com>
2025-01-27 19:10:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public interface ISelectable
+{
+    void OnSelect();
+}
+public interface IUpdatable
+{
+    void Refresh();
+}
+public class Screen : ISelectable, IUpdatable
+{
+    public void OnSelect()
+    {
+        Console.WriteLine("Screen has been selected.");
+    }
+
+    public void Refresh()
+    {
+        Console.WriteLine("Screen has been refreshed.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Screen screen = new Screen();
+
+        screen.OnSelect();
+        screen.Refresh();
+    }
+}
